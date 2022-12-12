07c8809a8d68f5aae124c788b3d9f1e50a5c5a20
C.Pontes <danieljo2014@outlook.com>
2022-12-12 20:02:21 +0000

    minor stuff. and restructuring

--- litestepooooo/litestepooooo.cpp
+++ litestepooooo/litestepooooo.cpp
@@ -5,7 +5,6 @@
 #include <CommCtrl.h>
 #include <dwmapi.h>
 #include "Styling.h"
-#include "autostart.h"
 #include "colors.h"
 #include "Services/TaskService.h"
 #include <stdlib.h>
@@ -19,6 +18,41 @@ std::wstring windowClassName{ L"LiteBoxDesktopWindoww" };
 CTaskbar* taskbar;
 HWND hDesktopWnd = NULL;
 HINSTANCE main_hInstance = NULL;
+
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
+void setAutostart(int on , int elevate)
+{
+    // Open key
+    HKEY key;
+    int error = RegCreateKeyEx(HKEY_CURRENT_USER , L"Software\\Microsoft\\Windows\\CurrentVersion\\Run" , 0 , NULL , 0 , KEY_SET_VALUE , NULL , &key , NULL);
+    if (error != ERROR_SUCCESS) {
+        MessageBoxA(NULL , ("RegCreateKeyEx(HKEY_CURRENT_USER,'Software\\Microsoft\\Windows\\CurrentVersion\\Run') Error opening reg" + error) , "error" , MB_OK);
+        return;
+    }
+    if (on) {
+        // Get path
+        wchar_t path[MAX_PATH] , value[MAX_PATH + 20];
+        GetModuleFileName(NULL , path , ARRAY_SIZE(path));
+        swprintf(value , ARRAY_SIZE(value) , L"\"%s\"%s" , path , (elevate ? L" -elevate" : L""));
+        // Set autostart
+        error = RegSetValueEx(key , windowClassName.c_str() , 0 , REG_SZ , (LPBYTE)value , (wcslen(value) + 1) * sizeof(value[0]));
+        if (error != ERROR_SUCCESS) {
+            MessageBoxA(NULL , "RegSetValueEx('APP_NAME'), LSetAutostart()" + error , "ERROR" , MB_OK);
+
+            return;
+        }
+    }
+    else {
+        // Remove
+        error = RegDeleteValue(key , windowClassName.c_str());
+        if (error != ERROR_SUCCESS) {
+            MessageBoxA(NULL , "RegSetValueEx('APP_NAME'), LSetAutostart()" + error , "ERROR" , MB_OK);
+            return;
+        }
+    }
+    // Close key
+    RegCloseKey(key);
+}
 
 BOOL isRunning()
 {
